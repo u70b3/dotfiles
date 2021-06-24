041a2dbb68f5479acf31a90d012b265919d24660
libingshuai <libingshuai@kuaishou.com>
2021-06-24 10:38:19 +0800

    feat: add init script

---
+++ init/install_package.sh
@@ -0,0 +1,10 @@
+apt install node zsh vim git wget curl 
+npm install -g tldr
+
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# autojump
+# zsh-syntax-highlighting
+# zsh-autosuggestions
+
+
